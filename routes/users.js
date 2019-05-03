92ae8c3bddc300265a2be110a01d6f5bf021f6d2
Jasper Tham <jaspertham98@gmail.com>
2019-05-03 15:07:36 +0800

    Register Field Check & Login Commit

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,46 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register',{title:'Register'});
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login',{title:'Login'});
+});
+
+router.post('/register', upload.single('profileimage'),function(req,res,next){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+
+	if(req.file){
+		console.log('Uploading File....');
+		var profileimage = req.file.filename;
+	}else{
+		console.log('No File Uploaded.');
+		var profileimage = 'noimage.jpg';
+	}
+
+	//Form Validator
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('name','Email field is required').notEmpty();
+	req.checkBody('email','Email is not valid').isEmail();
+	req.checkBody('username','Username field is required').notEmpty();
+	req.checkBody('password','Password field is required').notEmpty();
+	
+	//Check Error
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors: errors
+		});
+
+	}else{
+		console.log('No Errors');
+	}
+});
+
 
 module.exports = router;
